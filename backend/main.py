06b11c7629319bbca1749702f8dfbc1b3f047b30
Dodla Nikhil <nikhildodla276@gmail.com>
2026-06-16 13:36:01 +0000

    feat: add completion of phase 2 research_assistant

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
+from researcher import run_research
 
 load_dotenv()
 
@@ -12,7 +13,7 @@ class ResearchRequest(BaseModel):
 
 @app.post("/research")
 def research(request: ResearchRequest):
-    return {"received_topic": request.topic}
+    return {"report": run_research(request.topic)}
 
 @app.get("/health")
 def health_check():
--- backend/researcher.py
+++ backend/researcher.py
@@ -1,3 +1,13 @@
 import os
 from langchain_groq import ChatGroq
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import HumanMessage
+
+
+def run_research(topic:str) -> str:
+    llm = ChatGroq(model="llama-3.3-70b-versatile", api_key=os.getenv("GROQ_API_KEY"))
+    tool = TavilySearchResults(max_results=5)
+    results = tool.invoke(topic)
+    prompt = f"Research topic:{topic}\n\nSearch results:{str(results)}\n\nWrite a clean structured research report based on these results."
+    response = llm.invoke([HumanMessage(content=prompt)])
+    return response.content
