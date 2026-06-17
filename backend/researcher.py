3ab3e997afcabf96925a5a28ef406854bf4d25a6
Nikhil Dodla <nikhildodla276@gmail.com>
2026-06-17 23:12:07 +0530

    Update researcher.py

--- backend/researcher.py
+++ backend/researcher.py
@@ -3,6 +3,7 @@ from langchain_groq import ChatGroq
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.messages import HumanMessage
 
+memory = ConversationBufferMemory
 
 def run_research(topic:str) -> str:
     llm = ChatGroq(model="llama-3.3-70b-versatile", api_key=os.getenv("GROQ_API_KEY"))
